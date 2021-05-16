600bde32918cdf162c88b2876b6ae1b183cda064
JavaAction <yunus.torluoglu@hs-furtwangen.de>
2021-05-16 09:23:46 +0200

    New Functions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Person } from './person';
-
 import { PersonService } from './person.service';
+
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,9 @@ import { PersonService } from './person.service';
 })
 export class AppComponent implements OnInit
 {
-  public persons: Person[] = [];
- 
-
+  //variable that's goin to hold all of the persons that coming from the backend as Person Array
+  public persons: Person[] | undefined;
+  public editPerson: Person | undefined;
 
   constructor( private personsService: PersonService ) { }
 
@@ -31,8 +32,63 @@ export class AppComponent implements OnInit
       (error: HttpErrorResponse ) => {
         alert(error.message);
       }
-    )
+    );
   }
 
+  public onAddPerson( addForm: NgForm ): void 
+  {
+    //close the model
+    document.getElementById('add-person-form')?.click();
+    this.personsService.addPerson(addForm.value).subscribe( 
+      ( response: Person ) => {
+        console.log( response );
+        this.getPersons();
+        addForm.reset();
+      },
+      ( error: HttpErrorResponse ) => {
+        alert( error.message );
+        addForm.reset();
+      }
+     );
+  }
 
+  public onUpdatePerson( person: Person ): void 
+  {
+    this.personsService.updatePerson(person).subscribe( 
+      ( response: Person ) => {
+        console.log( response );
+        this.getPersons();
+      },
+      ( error: HttpErrorResponse ) => {
+        alert( error.message );
+      }
+     );
+  }
+
+  //pass a person and a mode (what exactly the user is trying to do)
+  public onOpenModal(person: Person, mode: string): void
+  {
+    //to access to the entire div in app.component.html
+    const container = document.getElementById( 'main-container' );
+    const button = document.createElement( 'button' );
+
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute( 'data-toggle', 'modal' );
+    if ( mode == 'add' ) 
+    {
+      button.setAttribute('data-target', '#addPersonModal' );
+    }
+    if ( mode == 'edit' ) 
+    {
+      this.editPerson = person;
+      button.setAttribute('data-target', '#updatePersonModal' );
+    }
+    if ( mode == 'delete' ) 
+    {
+      button.setAttribute('data-target', '#deletePersonModal' );
+    }
+    container?.appendChild( button );
+    button.click();
+  }
 }
--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,37 +1,40 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Person } from './person';
+import { environment } from 'src/environments/environment';
 
-@Injectable({providedIn: 'root'})
-export class PersonService {
-    //to pass the url, see in the below functions. An empty string, to pu the url later.
-    private apiServerUrl = '';
+//let the entire angular application app know about the service (the root component of our app)
+@Injectable({ providedIn: 'root' })
+export class PersonService 
+{
+  //to pass the url whcih was defined in the environment.ts
+  private apiServerUrl = environment.apiBaseUrl;
 
-    //this is a built-in http client that we can use to make http request and we can just injecet it in the surface so that we can use it.
-    constructor( private http: HttpClient ) {}
-    //interface for the request are going to be returning in Observable< GENERICS >, look persons.ts
-    // this is going to return an obserable of persons array
-    //returning a persons array of the javaScript perspective or from a 
-    //java perspective that's just going to be a list or any type of collection
-    public getPersons(): Observable<Person[]> 
-    {
-        //${...} this is an javascript notation that can be use to put variables, and then string at the same time
-        return this.http.get<Person[]>(`${this.apiServerUrl}/person/all`);                
-    }
+  constructor( private http: HttpClient ) { }
+  //interface for the request are going to be returning in Observable< GENERICS >, look persons.ts
+  // this is going to return an obserable of persons array
+  //returning a persons array of the javaScript perspective or from a 
+  //java perspective that's just going to be a list or any type of collection
+  public getPersons(): Observable<Person[]> 
+  {
+    //${...} this is an javascript notation that can be use to put variables, and then string at the same time
+    return this.http.get<Person[]>(`${this.apiServerUrl}/person/all`);                
+  }
 
-    public addPerson( person: Person ): Observable<Person> 
-    {
-        return this.http.post<Person>(`${this.apiServerUrl}/person/add`, person);                
-    }
+  public addPerson( person: Person ): Observable<Person> 
+  {
+      return this.http.post<Person>(`${this.apiServerUrl}/person/add`, person);                
+  }
 
-    public updatePerson( person: Person ): Observable<Person> 
-    {
-        return this.http.put<Person>(`${this.apiServerUrl}/person/update`, person);                
-    }
+  public updatePerson( person: Person ): Observable<Person> 
+  {
+      return this.http.put<Person>(`${this.apiServerUrl}/person/update`, person);                
+  }
 
-    public deletePerson( personId: number ): Observable<void> 
-    {
-        return this.http.delete<void>(`${this.apiServerUrl}/person/delete/${personId}`);                
-    }
+  public deletePerson( personId: number ): Observable<void> 
+  {
+      return this.http.delete<void>(`${this.apiServerUrl}/person/delete/${personId}`);                
+  }
+
 }
